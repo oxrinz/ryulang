cbb6bc62a6095028571c23410dd8e224d4f8a141
rinz <roberts.denisovs@gmail.com>
2025-06-15 09:38:28 +0300

    cpu flake

--- DEV.md
+++
@@ -1,5 +0,0 @@
-# Things to do:
-- Kernel splits, device specific
-    - Figure out a way to represent rirops in different kernels, nicely
-- Dashboard stage support
-- Support multiple outputs -> pass around an array of rirops instead of a single one
---
+++ flakes/cpu/flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "RyuNvidia";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+            cudaSupport = true;
+          };
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            gdb
+            zig
+            llvm
+            pkgconf
+            gcc13
+            clang
+          ];
+          shellHook = ''
+            export LD_LIBRARY_PATH=${pkgs.llvm}/lib:${pkgs.llvmPackages.mlir}/lib:$LD_LIBRARY_PATH
+            export LIBRARY_PATH=${pkgs.llvm}/lib:${pkgs.llvmPackages.mlir}/lib:$LIBRARY_PATH
+            export C_INCLUDE_PATH=${pkgs.llvm}/include:$C_INCLUDE_PATH
+            export LLVM_PATH=${pkgs.llvm}
+            export LLVM_CONFIG=${pkgs.llvm}/bin/llvm-config
+            export PATH=${pkgs.gcc13}/bin:$PATH
+            export PKG_CONFIG_PATH=${pkgs.llvm}/lib/pkgconfig:$PKG_CONFIG_PATH
+          '';
+        };
+      });
+}
